752d5379ecd1b6b1c991553c35f23d54a6c1a0c9
Koechian <ianovic8@live.com>
2022-11-23 21:30:29 +0300

    Login

---
+++ app/src/main/java/com/example/crafteducate/ui/login/Login.kt
@@ -0,0 +1,12 @@
+package com.example.crafteducate.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.crafteducate.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
